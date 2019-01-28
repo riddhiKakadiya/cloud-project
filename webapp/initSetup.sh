57f05f6d547fa0331be27cf82a0dd184e3cc6b9d
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-01-27 22:18:40 -0500

    adding migration code for django

--- webapp/initSetup.sh
+++ webapp/initSetup.sh
@@ -47,6 +47,8 @@ virtualenv -p python3 djangoEnv
 source djangoEnv/bin/activate
 pip install -r requirements.txt
 cd WebProject
+python3 manage.py makemigrations
+python3 manage.py migrate
 python3 manage.py runserver
 
 exit
