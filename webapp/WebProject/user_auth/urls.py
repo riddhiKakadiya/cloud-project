0cf6641498bff4110dbd0647557c81a97f128650
riddhiKakadiya <kamlesh.r@northeastern.edu>
2019-01-29 14:10:04 -0800

    Add: user registration

--- webapp/WebProject/user_auth/urls.py
+++ webapp/WebProject/user_auth/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    #path('register', views.registerPage, name='index'),
+    path('user/register', views.registerPage, name='registerPage'),
     path('test', views.testpage, name='index'),
 ]
