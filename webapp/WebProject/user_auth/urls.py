62fc2fb827027855469ac8c9badc09859567ecda
jai-soni <soni.j@northeastern.edu>
2019-01-29 23:17:29 -0800

    Add: adding functionality for basic authentication and get current time as response

--- webapp/WebProject/user_auth/urls.py
+++ webapp/WebProject/user_auth/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('test', views.testpage, name='index'),
+    path('signin', views.signin, name='signin'),
     path('user/register', views.registerPage, name='registerPage'),
 ]
--- webapp/WebProject/user_auth/views.py
+++ webapp/WebProject/user_auth/views.py
@@ -1,10 +1,13 @@
 #importing Django libraries
 from django.shortcuts import render
+from django.contrib.auth import authenticate
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User
 import json
 import re
+import base64
+import time
 
 #--------------------------------------------------------------------------------
 # Function definitions
@@ -83,3 +86,24 @@ def registerPage(request):
 				return JsonResponse({'message':username_status + " " + password_status})
 	return JsonResponse({'message':'Error : Please use a post method with parameters username and password to create user'})
 
+def testpage(request):
+    return HttpResponse("testpage" + password_check("testa"))
+
+def signin(request):
+    if 'HTTP_AUTHORIZATION' in request.META:
+        auth = request.META['HTTP_AUTHORIZATION'].split()
+        if len(auth) == 2:
+            if auth[0].lower() == "basic":
+                authstring = base64.b64decode(auth[1]).decode("utf-8")
+                username, password = authstring.split(':', 1)
+                user = authenticate(username=username, password=password)
+                if user is not None and user.is_staff:
+                # handle your view here
+                    #return render_to_response('my_template.html')
+                    return HttpResponse("Current time is : "+ time.ctime())
+
+    # otherwise ask for authentification
+    response = HttpResponse("")
+    response.status_code = 401
+    response['WWW-Authenticate'] = 'Basic realm="restricted area"'
+    return response
