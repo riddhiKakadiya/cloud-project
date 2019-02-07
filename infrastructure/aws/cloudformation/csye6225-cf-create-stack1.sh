a43cfc49f53370311b331d69f04581c1c7027db2
riddhiKakadiya <kamlesh.r@northeastern.edu>
2019-02-06 18:17:45 -0800

    updating json template

--- infrastructure/aws/cloudformation/csye6225-cf-create-stack1.sh
+++ infrastructure/aws/cloudformation/csye6225-cf-create-stack1.sh
@@ -1,5 +1,18 @@
+#import logging, boto3, time
+
+#boto3.set_stream_logger('boto3', level=boto3.logging.DEBUG)
+#boto3.set_stream_logger('botocore', level=boto3.logging.DEBUG)
+#boto3.set_stream_logger('boto3.resources', level=boto3.logging.DEBUG)
+
+#cf = boto3.client('cloudformation')
+
 #create vpc
 #!/bin/bash
+
+
+
+#create stack (dynamically)
+
 STACK_NAME=$1
 #Check if stack name isnt passed empty
 if [ $# -lt 1 ]; then
@@ -7,12 +20,15 @@ if [ $# -lt 1 ]; then
   exit 1
 fi
 
-#create stack (dynamically)
-stack_id=$(aws cloudformation create-stack --stack-name "$1" --template-body file://csye6225-cf-networking.json --parameters ParameterKey=Name,ParameterValue=$1)  
+stack_id=$(aws cloudformation create-stack --stack-name "$1" --template-body file://csye6225-cf-networking1.json)  
 echo "creating stack $1"
 echo "$stack_id"
-aws cloudformation wait stack-create-complete --stack-name $1 
 
+aws cloudformation wait stack-create-complete --stack-name $1 
+#cf.get_waiter('stack_create_complete').wait(StackName=$1)
 echo "created stack $1"
+
+
+
 
 
