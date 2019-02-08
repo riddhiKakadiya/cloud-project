aff385d2813c270851e923fbb015fad4acbb608d
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-02-07 23:50:08 -0500

    Add : Create Note Assignment 3

--- webapp/WebProject/user_auth/models.py
+++ webapp/WebProject/user_auth/models.py
@@ -1,3 +1,14 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class NotesModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=500)
+    content = models.CharField(max_length=500)
+    created_on = models.DateTimeField()
+    last_updated_on = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING)
+    def __str__(self):
+        return (str(self.id))
+
