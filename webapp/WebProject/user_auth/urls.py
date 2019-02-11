e5aad967a4209e31ddc248b66e9ba4e48e465bab
riddhiKakadiya <kamlesh.r@northeastern.edu>
2019-02-11 10:32:32 -0800

    merge

--- webapp/WebProject/user_auth/urls.py
+++ webapp/WebProject/user_auth/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     re_path(r'^user/register/?$', views.registerPage, name='registerPage'),
     re_path(r'^note$', views.createNotes, name='registerPage'),
     re_path(r'^note/(?P<note_id>[0-9a-z-]+)$', views.getNotes, name='registerPage')
-#	 re_path(r'^note/(?P<note_id>[0-9a-z-]+)$', views.updateNotes, name='updateNote')
+#    re_path(r'^note/(?P<note_id>[0-9a-z-]+)$', views.updateNotes, name='updateNote')
 ]
--- webapp/WebProject/user_auth/views.py
+++ webapp/WebProject/user_auth/views.py
@@ -117,6 +117,7 @@ def signin(request):
 		auth = request.META['HTTP_AUTHORIZATION'].split()
 		if len(auth) == 2:
 			if auth[0].lower() == "basic":
+
 				authstring = base64.b64decode(auth[1]).decode("utf-8")
 				username, password = authstring.split(':', 1)
 				if not username and not password:
@@ -164,21 +165,21 @@ def createNotes(request):
 				return JsonResponse(message_list, status=201, safe=False)
 	return JsonResponse({'message': 'Error : Incorrect user details'}, status=401)
 
-#@csrf_exempt
-#def updateNotes(request, id):
-#	if request.method == 'POST':
-#		note = NotesModel.objects.filter(id=id)
-#		received_json_data = json.loads(request.body.decode("utf-8"))
-#		note.title = received_json_data['title']
+# @csrf_exempt
+# def updateNotes(request, note_id):
+# 	if request.method == 'POST':
+# 		note = NotesModel.objects.filter(id=note_id, user=user)
+# 		received_json_data = json.loads(request.body.decode("utf-8"))
+# 		note.title = received_json_data['title']
 # 		note.content = received_json_data['content']
 # 		note.last_updated_on = datetime.datetime.now()
-#		
+		
 # 		user = validateSignin(request.META)
 # 		if(user):
 # 			note.save()
-#			message = "Note updated!""
-#			return JsonResponse(message, status=201)
-#
+# 			message = "Note updated!"
+# 			return JsonResponse(message, status=201)
+
 #
 #else
 #	user = validateSignin(request.META)
