0d6de86709b97c410edd44987f5bd63228e95c93
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-02-11 12:50:03 -0500

    Edit : pushing check uuid to function side of the views

--- webapp/WebProject/user_auth/views.py
+++ webapp/WebProject/user_auth/views.py
@@ -68,6 +68,32 @@ def validateUserName(username):
         return True
     return "* please enter valid email ID *"
 
+def is_valid_uuid(uuid_to_test, version=4):
+    """
+    Check if uuid_to_test is a valid UUID.
+
+    Parameters
+    ----------
+    uuid_to_test : str
+    version : {1, 2, 3, 4}
+
+    Returns
+    -------
+    `True` if uuid_to_test is a valid UUID, otherwise `False`.
+
+    Examples
+    --------
+    >>> is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a')
+    True
+    >>> is_valid_uuid('c9bf9e58')
+    False
+    """
+    try:
+        uuid_obj = UUID(uuid_to_test, version=version)
+    except:
+        return False
+
+    return str(uuid_obj) == uuid_to_test
 
 # --------------------------------------------------------------------------------
 # Views definitions
@@ -197,30 +223,3 @@ def getNoteFromId(request, note_id=""):
             return JsonResponse({'message': 'Error : Invalid Note ID'}, status=400)
     return JsonResponse({'message': 'Error : Incorrect user details'}, status=401)
 
-
-def is_valid_uuid(uuid_to_test, version=4):
-    """
-    Check if uuid_to_test is a valid UUID.
-
-    Parameters
-    ----------
-    uuid_to_test : str
-    version : {1, 2, 3, 4}
-
-    Returns
-    -------
-    `True` if uuid_to_test is a valid UUID, otherwise `False`.
-
-    Examples
-    --------
-    >>> is_valid_uuid('c9bf9e57-1685-4c89-bafb-ff5af830be8a')
-    True
-    >>> is_valid_uuid('c9bf9e58')
-    False
-    """
-    try:
-        uuid_obj = UUID(uuid_to_test, version=version)
-    except:
-        return False
-
-    return str(uuid_obj) == uuid_to_test
