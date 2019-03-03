cf7c5550cbef1e2f37c4973baa919f46dd4c50d3
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-03 15:12:32 -0500

    circle CI : test34

--- webapp/startServer_centos.sh
+++ webapp/startServer_centos.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 cd WebProject
 sudo scl enable rh-python36 bash
-sudo rm -rf djangoEnv
-virtualenv -p python djangoEnv
-source djangoEnv/bin/activate
-cat requirements.txt | xargs -n 1 pip install
-python manage.py runserver 0.0.0.0:80
+source WebProject/djangoEnv/bin/activate
+cat WebProject/requirements.txt | xargs -n 1 pip install
+python WebProject/manage.py runserver 0.0.0.0:80
