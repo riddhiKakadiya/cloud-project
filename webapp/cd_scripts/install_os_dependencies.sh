a787d7c7ad3f9822149901403607ad6ffc6fa702
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-03 18:24:33 -0500

    circle CI : test59

--- webapp/cd_scripts/install_os_dependencies.sh
+++ webapp/cd_scripts/install_os_dependencies.sh
@@ -2,5 +2,5 @@
 sudo yum install epel-release -y
 sudo yum makecache
 sudo yum search pip | grep python3
-sudo yum install python34-pip -y
+sudo yum install python36-pip -y
 sudo pip3 install virtualenv
