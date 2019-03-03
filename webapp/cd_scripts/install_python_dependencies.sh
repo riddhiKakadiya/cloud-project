2a3d945492793c518037afe3a044a95828dad0a1
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-03 18:21:05 -0500

    circle CI : test58

--- webapp/cd_scripts/install_python_dependencies.sh
+++ webapp/cd_scripts/install_python_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo scl enable rh-python36 bash
 sudo chown centos:centos /home/centos/webapp
+sudo chown -R centos:centos /home/centos/webapp/*
 virtualenv /home/centos/webapp/WebProject/djangoEnv
 sudo chown centos:centos /home/centos/webapp/WebProject/djangoEnv/*
 source /home/centos/webapp/WebProject/djangoEnv/bin/activate
