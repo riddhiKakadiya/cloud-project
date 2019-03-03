8b023c3c2da4979cc1d8643f7820d1887c441b00
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-03 12:13:45 -0500

    Add : circle ci basic

---
+++ webapp/WebProject/djangoEnv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sreeragsreenath/cloud/csye6225-spring2019/webapp/WebProject/djangoEnv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
