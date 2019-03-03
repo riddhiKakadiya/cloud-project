198e51770a288a75cdfe3769eabece326b253f3d
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-03 15:29:56 -0500

    circle CI : test37

--- webapp/startServer_centos.sh
+++ webapp/startServer_centos.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+ls
 sudo scl enable rh-python36 bash
 source WebProject/djangoEnv/bin/activate
-python WebProject/WebProject/manage.py runserver 0.0.0.0:80
+python WebProject/WebProject/manage.py runserver 0.0.0.0:80 --settings=WebProject.settings_test
