c8c86f68e7a199fc28b6422b4e82c77e40891609
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-04 17:25:08 -0500

    fix 6

--- webapp/cd_scripts/install_os_dependencies.sh
+++ webapp/cd_scripts/install_os_dependencies.sh
@@ -8,13 +8,14 @@
 # sudo yum install python36-pip -y
 # sudo easy_install-3.6 pip
 # sudo scl enable rh-python36 bash
-# pip3 install virtualenv
+# 
 sudo mv /home/centos/webapp/cd_scripts/gunicorn.service /etc/systemd/system/gunicorn.service
 sudo mv /home/centos/webapp/cd_scripts/nginx.conf /etc/nginx/nginx.conf
 sudo mkdir --parents /etc/systemd/system/nginx.service.d/; sudo mv /home/centos/webapp/cd_scripts/override.conf $_
 sudo scl enable rh-python36 bash
+pip3 install virtualenv
+virtualenv -p python3.6 /home/centos/webapp/WebProject/djangoEnv
 sudo chown centos:centos /home/centos/webapp
 sudo chown -R centos:centos /home/centos/webapp/*
 sudo chown centos:centos /home/centos/webapp/WebProject/djangoEnv/*
-virtualenv -p python3.6 /home/centos/webapp/WebProject/djangoEnv
 
