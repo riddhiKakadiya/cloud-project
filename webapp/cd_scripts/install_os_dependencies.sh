27ee8569a87907f3e51a8e551b7c7ff7604abece
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-04 07:17:20 -0500

    Add : auto create config files

--- webapp/cd_scripts/install_os_dependencies.sh
+++ webapp/cd_scripts/install_os_dependencies.sh
@@ -4,3 +4,5 @@ sudo yum makecache
 sudo yum search pip | grep python3
 sudo yum install python36-pip -y
 sudo pip3 install virtualenv
+sudo mv /home/centos/webapp/cd_scripts/gunicorn.service /etc/systemd/system/gunicorn.service
+sudo mv /home/centos/webapp/cd_scripts/nginx.conf /etc/nginx/nginx.conf
