f447769f48957930f6dcd0f24279a6693a83473e
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-03 20:32:44 -0500

    circle CI : test61

--- webapp/cd_scripts/start_server.sh
+++ webapp/cd_scripts/start_server.sh
@@ -1,3 +1,30 @@
 #!/bin/bash
 source /home/centos/webapp/WebProject/djangoEnv/bin/activate
-python3 /home/centos/webapp/WebProject/manage.py runserver 0.0.0.0:8000 --settings=WebProject.settings_test
+python3 /home/centos/webapp/WebProject/manage.py collectstatic
+cd /home/centos/webapp/WebProject
+sudo semanage permissive -a httpd_t
+sudo systemctl daemon-reload
+sudo systemctl restart gunicorn
+sudo systemctl enable gunicorn
+sudo usermod -a -G centos nginx
+chmod 710 /home/centos
+sudo nginx -t
+sudo systemctl daemon-reload
+sudo systemctl restart nginx
+sudo systemctl enable nginx
+
+# sudo rm /etc/systemd/system/gunicorn.service
+# sudo nano /etc/systemd/system/gunicorn.service
+
+# sudo rm /etc/nginx/nginx.conf
+# sudo nano /etc/nginx/nginx.conf
+
+
+
+# sudo systemctl daemon-reload
+# sudo systemctl restart nginx
+# sudo systemctl enable nginx
+# sudo systemctl daemon-reload
+# sudo systemctl restart gunicorn
+# sudo systemctl enable gunicorn
+# python3 /home/centos/webapp/WebProject/manage.py runserver 0.0.0.0:8000 --settings=WebProject.settings_test
