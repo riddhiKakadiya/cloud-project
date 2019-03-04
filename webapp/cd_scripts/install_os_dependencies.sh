03b1c1166f8cd08608d17785757638b8def060eb
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-04 07:41:43 -0500

    Fix : pip3 issue test1

--- webapp/cd_scripts/install_os_dependencies.sh
+++ webapp/cd_scripts/install_os_dependencies.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 sudo yum install epel-release -y
 sudo yum makecache
-sudo yum search pip | grep python3
-sudo yum install python36-setuptools -y
-sudo yum install python36-pip -y
-sudo easy_install-3.6 pip
-sudo pip3 install virtualenv
 sudo mv /home/centos/webapp/cd_scripts/gunicorn.service /etc/systemd/system/gunicorn.service
 sudo mv /home/centos/webapp/cd_scripts/nginx.conf /etc/nginx/nginx.conf
-sudo mv /home/centos/webapp/cd_scripts/override.conf /etc/systemd/system/nginx.service.d/override.conf
+sudo mkdir --parents /etc/systemd/system/nginx.service.d/; sudo mv /home/centos/webapp/cd_scripts/override.conf $_
