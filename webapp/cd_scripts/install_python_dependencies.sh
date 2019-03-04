5f9fa48905cf6aa3419b0d12a1c79a87bc3050bf
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-04 18:04:08 -0500

    fix 8

--- webapp/cd_scripts/install_python_dependencies.sh
+++ webapp/cd_scripts/install_python_dependencies.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo scl enable rh-python36 bash "source /home/centos/webapp/WebProject/djangoEnv/bin/activate && pip3 install -r /home/centos/webapp/WebProject/requirements.txt"
+sudo scl enable rh-python36 "source /home/centos/webapp/WebProject/djangoEnv/bin/activate && pip3 install -r /home/centos/webapp/WebProject/requirements.txt"
