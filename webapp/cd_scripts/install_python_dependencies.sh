5edc9d241a96a37a451059e8793a4b9f6d908657
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-04 08:26:10 -0500

    Fix : pip3 issue test9

--- webapp/cd_scripts/install_python_dependencies.sh
+++ webapp/cd_scripts/install_python_dependencies.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-sudo yum install rh-python36 -y
-sudo scl enable rh-python36 bash
-sudo yum groupinstall 'Development Tools' -y
-sudo pip3 install virtualenv
+# sudo yum install rh-python36 -y
+# sudo scl enable rh-python36 bash
+# sudo yum groupinstall 'Development Tools' -y
+# sudo pip3 install virtualenv
 sudo chown centos:centos /home/centos/webapp
 sudo chown -R centos:centos /home/centos/webapp/*
 virtualenv -p python3.6 /home/centos/webapp/WebProject/djangoEnv
-sudo chown centos:centos /home/centos/webapp/WebProject/djangoEnv/*
-source /home/centos/webapp/WebProject/djangoEnv/bin/activate
-pip3 install -r /home/centos/webapp/WebProject/requirements.txt
+
+# sudo chown centos:centos /home/centos/webapp/WebProject/djangoEnv/*
+# source /home/centos/webapp/WebProject/djangoEnv/bin/activate
+# pip3 install -r /home/centos/webapp/WebProject/requirements.txt
