8575f0bda04a826ed5cdffbf1120a31e8a17c496
riddhiKakadiya <kamlesh.r@northeastern.edu>
2019-03-11 15:50:47 -0400

    cicd.yaml

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -1,8 +1,4 @@
-echo "Enter the stack name(cicd)"
-read Stack_Name
-
-echo -e "\n"
-echo "Enter the Deployment group name, Code Deploy application name, S3 Bucket Name, Tag Key & Tag value of the ec2 instance you want to connect to deployment group."
+echo "Enter the stack name(cicd),Deployment group name, Code Deploy application name, S3 Bucket Name, Tag Key & Tag value of the ec2 instance you want to connect to deployment group."
 
 aws cloudformation create-stack 
-	--stack-name "$1" --template-body file://csye6225-cf-networking.yaml --capabilities CAPABILITY_NAMED_IAM --parameters  ParameterKey="$2",ParameterValue="$3" ParameterKey="$4",ParameterValue="$5" ParameterKey="S3bucketname",ParameterValue="$" ParameterKey="$",ParameterValue=$Tag_key ParameterKey="TagValue",ParameterValue=$Tag_value --disable-rollback
+	--stack-name "$1" --template-body file://csye6225-cf-networking.yaml --capabilities CAPABILITY_NAMED_IAM --parameters  ParameterKey="Deploymentgroupname",ParameterValue="$2" ParameterKey="codedeployapplicationname",ParameterValue="$3" ParameterKey="S3bucketname",ParameterValue="$4" ParameterKey="TagKey",ParameterValue="$5" ParameterKey="TagValue",ParameterValue="$6" --disable-rollback
