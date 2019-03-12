3df461f63c96ba8d4501466419c9998c3d8970a8
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-12 13:08:52 -0400

    Add : adding jq to install

--- webapp/cd_scripts/install_python_dependencies.sh
+++ webapp/cd_scripts/install_python_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo scl enable rh-python36 "source /home/centos/webapp/WebProject/djangoEnv/bin/activate && pip3 install -r /home/centos/webapp/WebProject/requirements.txt"
-sudo export S3_BUCKET=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | startswith("code-deploy")).Name')
+export S3_BUCKET=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | startswith("code-deploy")).Name')
