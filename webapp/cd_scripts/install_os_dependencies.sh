e90721e47bac04908d4aeb79540cc93a8270a060
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-12 21:55:06 -0400

    Test : code deploy 3

--- webapp/cd_scripts/install_os_dependencies.sh
+++ webapp/cd_scripts/install_os_dependencies.sh
@@ -2,8 +2,5 @@
 sudo mv /home/centos/webapp/cd_scripts/gunicorn.service /etc/systemd/system/gunicorn.service
 sudo mv /home/centos/webapp/cd_scripts/nginx.conf /etc/nginx/nginx.conf
 sudo mkdir --parents /etc/systemd/system/nginx.service.d/; sudo mv /home/centos/webapp/cd_scripts/override.conf $_
-sudo scl enable rh-python36 "virtualenv -p python3.6 /home/centos/webapp/WebProject/djangoEnv"
-sudo chown centos:centos /home/centos/webapp
-sudo chown -R centos:centos /home/centos/webapp/*
-sudo chown centos:centos /home/centos/webapp/WebProject/djangoEnv/
-sudo chown centos:centos /home/centos/webapp/WebProject/djangoEnv/*
+sudo yum install jq -y
+
