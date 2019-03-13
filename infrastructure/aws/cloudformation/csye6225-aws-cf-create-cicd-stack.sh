062cd3abbce25c13f4b1ebe456195d8309433830
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-13 17:05:55 -0400

    Add : CF for Circle CI User

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -1,4 +1,25 @@
 echo "Enter the stack name(cicd),Deployment group name, Code Deploy application name, aws account id, Tag Key & Tag value of the ec2 instance you want to connect to deployment group."
 
-aws cloudformation create-stack 
-	--stack-name "$1" --template-body file://csye6225-cf-networking.yaml --capabilities CAPABILITY_NAMED_IAM --parameters  ParameterKey="Deploymentgroupname",ParameterValue="$2" ParameterKey="codedeployapplicationname",ParameterValue="$3" ParameterKey="aws_account_id",ParameterValue="$4" ParameterKey="TagKey",ParameterValue="$5" ParameterKey="TagValue",ParameterValue="$6" --disable-rollback
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+AWS_REGION="us-east-1"
+CODE_DEPLOY_APPLICATION_NAME="csye6225-webapp"
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
+S3_BUCKET=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | startswith("code-deploy")).Name')
+
+aws cloudformation create-stack --stack-name "$1" --template-body file://csye-cf-cicd.yaml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=AWSAccountId,ParameterValue=$AWS_ACCOUNT_ID ParameterKey=CodeDeployApplicationName,ParameterValue=$CODE_DEPLOY_APPLICATION_NAME ParameterKey=AWSRegion,ParameterValue=$AWS_REGION ParameterKey=S3BucketName,ParameterValue=$S3_BUCKET
+
+echo "Waiting for Stack $1 to be created"
+echo "$response"
+aws cloudformation wait stack-create-complete --stack-name $1
+echo "Stack $1 created successfully"
+
+if [ $? = "0" ]
+then
+	aws iam create-access-key --user-name CircleCI1
+else
+	echo "Error : Creating user"
+	exit
+fi
+echo "Bucket Name : "
+echo $S3_BUCKET
+
