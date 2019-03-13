3cd3e2c1de501eec2e45760d2b7aa76fdf5959f2
riddhiKakadiya <kamlesh.r@northeastern.edu>
2019-03-13 17:17:52 -0400

    add:code seploy app & group

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -2,3 +2,8 @@ echo "Enter the stack name(cicd),Deployment group name, Code Deploy application 
 
 aws cloudformation create-stack 
 	--stack-name "$1" --template-body file://csye6225-cf-networking.yaml --capabilities CAPABILITY_NAMED_IAM --parameters  ParameterKey="Deploymentgroupname",ParameterValue="$2" ParameterKey="codedeployapplicationname",ParameterValue="$3" ParameterKey="aws_account_id",ParameterValue="$4" ParameterKey="TagKey",ParameterValue="$5" ParameterKey="TagValue",ParameterValue="$6" --disable-rollback
+
+#create code deploy application and group
+aws deploy create-application --application-name CodeDeployGitHubDemo-App
+
+aws deploy create-deployment-group --application-name csye6225-webapp --ec2-tag-filters Key=ec2-tag-key,Type=KEY_AND_VALUE,Value=ec2-tag-value --on-premises-tag-filters Key=on-premises-tag-key,Type=KEY_AND_VALUE,Value=on-premises-tag-value --deployment-group-name csye6225-webapp-deployment --service-role-arn service-role-arn
