5b6441b4c0528859ea395872a95e75ba070fcb20
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-20 18:20:58 -0400

    Assignment6 : Test

--- webapp/cd_scripts/clean_server.sh
+++ webapp/cd_scripts/clean_server.sh
@@ -3,3 +3,4 @@ sudo systemctl stop gunicorn
 sudo systemctl stop nginx
 sudo chown centos:centos /home/centos/webapp/*
 sudo rm -rf /home/centos/webapp
+sudo rm -rf /opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.toml
