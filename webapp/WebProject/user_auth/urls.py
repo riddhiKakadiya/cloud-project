81ed55765cb3be1057f6ac8a6dcccc30ed872f91
riddhiKakadiya <kamlesh.r@northeastern.edu>
2019-03-21 16:43:05 -0400

    updating after assignment5 review

--- webapp/WebProject/user_auth/urls.py
+++ webapp/WebProject/user_auth/urls.py
@@ -5,10 +5,10 @@ from . import views
 urlpatterns = [
     re_path(r'^$', views.signin, name='signin'),
     re_path(r'^user/register/?$', views.registerPage, name='registerPage'),
-    re_path(r'^note-riddhi/?$', views.createOrGetNotes, name='createOrGetNotes'),
-    re_path(r'^note-riddhi/(?P<note_id>[0-9a-z-]+)$', views.noteFromId, name='noteFromId'),
-    re_path(r'^note-riddhi/(?P<note_id>[0-9a-z-]+)/attachments$', views.addAttachmentToNotes, name='addAttachmentToNotes'),
-    re_path(r'^note-riddhi/(?P<note_id>[0-9a-z-]+)/attachments/(?P<attachment_id>[0-9a-z-]+)$', views.updateOrDeleteAttachments, name='updateOrDeleteAttachments')
+    re_path(r'^note/?$', views.createOrGetNotes, name='createOrGetNotes'),
+    re_path(r'^note/(?P<note_id>[0-9a-z-]+)$', views.noteFromId, name='noteFromId'),
+    re_path(r'^note/(?P<note_id>[0-9a-z-]+)/attachments$', views.addAttachmentToNotes, name='addAttachmentToNotes'),
+    re_path(r'^note/(?P<note_id>[0-9a-z-]+)/attachments/(?P<attachment_id>[0-9a-z-]+)$', views.updateOrDeleteAttachments, name='updateOrDeleteAttachments')
 #get, update, delete
     ]
     
