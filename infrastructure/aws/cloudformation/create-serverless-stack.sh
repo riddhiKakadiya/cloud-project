208a2b3072aa98215a4fdc54a66e8ca37e52cc8a
jai-soni <soni.j@northeastern.edu>
2019-03-24 21:06:36 -0700

    Add: Policy update to circleci, adding access for lambda bucket

---
+++ infrastructure/aws/cloudformation/create-serverless-stack.sh
@@ -0,0 +1,23 @@
+##create-serverless-stack.sh
+# echo "Fetching domain name from Route 53"
+# DOMAIN_NAME=$(aws route53 list-hosted-zones --query HostedZones[0].Name --output text)
+# DOMAIN_NAME="${DOMAIN_NAME%?}"
+# echo "$DOMAIN_NAME"
+
+LAMBDA_BUCKET=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | startswith("lambda")).Name')
+echo "LAMBDA_BUCKET: $LAMBDA_BUCKET"
+# LAMBDABUCKET="lambda.$DOMAIN_NAME"
+# echo "LAMBDA_BUCKET:- $LAMBDABUCKET"
+
+AccountId=$(aws iam get-user|python -c "import json as j,sys;o=j.load(sys.stdin);print o['User']['Arn'].split(':')[4]")
+echo "AccountId: $AccountId"
+
+SNSTOPIC_ARN="arn:aws:sns:us-east-1:$AccountId:SNSTopicResetPassword"
+echo "SNSTOPIC_ARN: $SNSTOPIC_ARN"
+
+aws cloudformation create-stack --stack-name "serverless" --capabilities "CAPABILITY_NAMED_IAM" --template-body file://./serverless.yaml --parameters ParameterKey=LAMBDABUCKET,ParameterValue=$LAMBDABUCKET ParameterKey=SNSTOPICARN,ParameterValue=$SNSTOPIC_ARN
+aws cloudformation wait stack-create-complete --stack-name "serverless"
+STACKDETAILS=$(aws cloudformation describe-stacks --stack-name "serverless" --query Stacks[0].StackId --output text)
+
+
+# exit 0
--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-cicd-stack.sh
@@ -5,8 +5,9 @@ AWS_REGION="us-east-1"
 CODE_DEPLOY_APPLICATION_NAME="csye6225-webapp"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity | jq -r '.Account')
 S3_BUCKET=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | startswith("code-deploy")).Name')
+LAMBDA_BUCKET=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | startswith("lambda")).Name')
 
-aws cloudformation create-stack --stack-name "$1" --template-body file://csye-6225-cf-cicd.yaml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=AWSAccountId,ParameterValue=$AWS_ACCOUNT_ID ParameterKey=CodeDeployApplicationName,ParameterValue=$CODE_DEPLOY_APPLICATION_NAME ParameterKey=AWSRegion,ParameterValue=$AWS_REGION ParameterKey=S3BucketNameCD,ParameterValue=$S3_BUCKET
+aws cloudformation create-stack --stack-name "$1" --template-body file://csye-6225-cf-cicd.yaml --capabilities CAPABILITY_NAMED_IAM --parameters ParameterKey=AWSAccountId,ParameterValue=$AWS_ACCOUNT_ID ParameterKey=CodeDeployApplicationName,ParameterValue=$CODE_DEPLOY_APPLICATION_NAME ParameterKey=AWSRegion,ParameterValue=$AWS_REGION ParameterKey=S3BucketNameCD,ParameterValue=$S3_BUCKET ParameterKey=LambdaBucketNameCD,ParameterValue=$LAMBDA_BUCKET
 
 echo "Waiting for Stack $1 to be created"
 echo "$response"
