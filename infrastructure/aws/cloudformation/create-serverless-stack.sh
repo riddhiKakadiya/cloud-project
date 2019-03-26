e23a629671d6476ac4c74dc48fe900ee52cc92af
Sreerag Mandakathil Sreenath <mandakathil.s@husky.neu.edu>
2019-03-26 19:10:44 -0400

    Fix : account id from python to jq

--- infrastructure/aws/cloudformation/create-serverless-stack.sh
+++ infrastructure/aws/cloudformation/create-serverless-stack.sh
@@ -3,7 +3,7 @@
 LAMBDA_BUCKET=$(aws s3api list-buckets | jq -r '.Buckets[] | select(.Name | startswith("lambda")).Name')
 echo "LAMBDA_BUCKET: $LAMBDA_BUCKET"
 
-AccountId=$(aws iam get-user|python -c "import json as j,sys;o=j.load(sys.stdin);print o['User']['Arn'].split(':')[4]")
+AccountId=$(aws sts get-caller-identity | jq -r '.Account')
 echo "AccountId: $AccountId"
 
 SNSTOPIC_ARN="arn:aws:sns:us-east-1:$AccountId:SNSTopicResetPassword"
@@ -13,5 +13,5 @@ aws cloudformation create-stack --stack-name "serverless" --capabilities "CAPABI
 aws cloudformation wait stack-create-complete --stack-name "serverless"
 STACKDETAILS=$(aws cloudformation describe-stacks --stack-name "serverless" --query Stacks[0].StackId --output text)
 
-echo "Stack serverless created successfully"
+# echo "Stack serverless created successfully"
 exit 0
