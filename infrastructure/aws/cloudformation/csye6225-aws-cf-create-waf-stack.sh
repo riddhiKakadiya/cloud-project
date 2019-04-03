af45c6fa1741e648ab2c4df29616551c2c6c15f7
jai-soni <soni.j@northeastern.edu>
2019-04-03 11:05:29 -0700

    Fix: Uodates to waf stack-attach load balancer arn, Fix: password Reset handle for request method other than POST

--- infrastructure/aws/cloudformation/csye6225-aws-cf-create-waf-stack.sh
+++ infrastructure/aws/cloudformation/csye6225-aws-cf-create-waf-stack.sh
@@ -2,10 +2,13 @@
 
 #Exit immediately if a command exits with a non-zero exit status.
 set -e
+##Get Load balacer ARN
+LOADBALANCER=$(aws elbv2 describe-load-balancers --query LoadBalancers[0].LoadBalancerArn --output text)
 
+echo "ELBResourceARN: $LOADBALANCER"
 ##Creating Stack
 echo "Creating Stack waf"
-response=$(aws cloudformation create-stack --stack-name "waf" --template-body file://owasp_10_base.yml) 
+response=$(aws cloudformation create-stack --stack-name "waf" --template-body file://owasp_10_base.yml --parameters ParameterKey=LOADBALANCER,ParameterValue=$LOADBALANCER) 
 #response=$(aws cloudformation create-stack --stack-name "$1" --template-body file://csye6225-cf-networking.yaml)
 echo "Waiting for Stack waf to be created"
 echo "$response"
