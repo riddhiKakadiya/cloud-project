e2b30adce89456444147fc9bbfa76d548085e960
jai-soni <soni.j@northeastern.edu>
2019-04-03 20:37:19 -0700

    Fix: bug fix

--- webapp/WebProject/WebProject/settings_default.py
+++ webapp/WebProject/WebProject/settings_default.py
@@ -42,11 +42,11 @@ config.read(pathToConfig)
 SECRET_KEY = 'l_k3zyn7$2j*vsvk&m3t5&*bp++r*=v*$c9gmoiy9z0xk5u_6m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SNSTOPICARN= config['Config']['SNSTOPICARN']
 DOMAIN_NAME= config['Config']['DOMAIN_NAME']
-ALLOWED_HOSTS = [DOMAIN_NAME]
+ALLOWED_HOSTS = ['*']
 
 # Define whether to run in dev environment or default(local) environment
 PROFILE = 'default'
