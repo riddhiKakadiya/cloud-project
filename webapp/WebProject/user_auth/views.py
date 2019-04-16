2d5137ad58ec556bf7051bd5778f2af22a1fb936
riddhiKakadiya <kamlesh.r@northeastern.edu>
2019-04-16 18:07:50 -0400

    timestamp

--- webapp/WebProject/user_auth/views.py
+++ webapp/WebProject/user_auth/views.py
@@ -325,8 +325,9 @@ def signin(request):
 						return JsonResponse({'message': 'Error : User not logged, Please provide credentials'}, status=401)
 					user = authenticate(username=username, password=password)
 					if user is not None:
-						current_time = datetime.datetime.now()
-						return JsonResponse({"current time": current_time.strftime("%y")})
+						d = {}
+						current_time = datetime.datetime.now().date()
+						return JsonResponse({"current time": current_time})
 		# otherwise ask for authentification
 		return JsonResponse({'message': 'Error : Incorrect user details entered'}, status=401)
 	else:
